d322e2b994196a3cdab6ff2ba2f2dc6d9cbed09c
Andrija Petrovic <andrija.hers@gmail.com>
2020-03-16 16:58:45 +0000

    Init

---
+++ index.js
@@ -0,0 +1,52 @@
+function createLib (execlib) {
+  return {
+    fetchProfile: [{
+      title: 'User Name',
+      type: 'string'
+    }],
+    getCandidates: [{
+      title: 'User Name',
+      type: 'string'
+    },{
+      title: 'Filter',
+      type: ['array', 'null']
+    }],
+    initiateRelation: [{
+      title: 'Initiator Name',
+      type: 'string'
+    },{
+      title: 'Target Name',
+      type: 'string'
+    }],
+    blockRelation: [{
+      title: 'Initiator Name',
+      type: 'string'
+    },{
+      title: 'Target Name',
+      type: 'string'
+    }],
+    getInitiators: [{
+      title: 'User Name',
+      type: 'string'
+    }],
+    acceptRelation: [{
+      title: 'Target name',
+      type: 'string'
+    },{
+      title: 'Initiator name',
+      type: 'string'
+    }],
+    dropRelation: [{
+      title: 'Initiator Name',
+      type: 'string'
+    },{
+      title: 'Target Name',
+      type: 'string'
+    }],
+    getMatches: [{
+      title: 'User Name',
+      type: 'string'
+    }]
+  };
+}
+module.exports = createLib;
