f99e3f73e4d2e85b9615b75f6b028a1875059c26
Andrija Petrovic <andrija.hers@gmail.com>
2020-03-18 14:02:22 +0000

    + web_component

---
+++ dist/browserified.js
@@ -0,0 +1,31 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+module.exports = {
+  user: {
+    user: {
+      getCandidates: [{
+        title: 'Filter',
+        type: ['array', 'null']
+      }],
+      initiateRelation: [{
+        title: 'Target Name',
+        type: 'string'
+      }],
+      blockRelation: [{
+        title: 'Target Name',
+        type: 'string'
+      }],
+      getInitiators: true,
+      acceptRelation: [{
+        title: 'Initiator name',
+        type: 'string'
+      }],
+      dropRelation: [{
+        title: 'Target Name',
+        type: 'string'
+      }],
+      getMatches: true
+    }
+  }
+}
+
+},{}]},{},[1]);
---
+++ webindex.js
@@ -0,0 +1,28 @@
+module.exports = {
+  user: {
+    user: {
+      getCandidates: [{
+        title: 'Filter',
+        type: ['array', 'null']
+      }],
+      initiateRelation: [{
+        title: 'Target Name',
+        type: 'string'
+      }],
+      blockRelation: [{
+        title: 'Target Name',
+        type: 'string'
+      }],
+      getInitiators: true,
+      acceptRelation: [{
+        title: 'Initiator name',
+        type: 'string'
+      }],
+      dropRelation: [{
+        title: 'Target Name',
+        type: 'string'
+      }],
+      getMatches: true
+    }
+  }
+}
